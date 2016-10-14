ad8c6c86a773e7d26f76d570e9ec885bb3aa32a4
Alsu Islamova <Alsu_Islamova@epam.com>
2016-10-14 20:27:31 +0600

    Add HTTP task

---
+++ LinksSearch/Links.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using HtmlAgilityPack;
+
+namespace LinksSearch
+{
+    public class Links
+    {
+        private string url;
+        private string location;
+
+        public Links(string url, string location)
+        {
+            this.url = url;
+            this.location = location;
+        }
+        public List<string>  GetLinksFromWebsite()
+        {
+            var doc = new HtmlWeb().Load(url);
+            var linkTags = doc.DocumentNode.Descendants("link");
+            var linkedPages = doc.DocumentNode.Descendants("a")
+                                              .Select(a => a.GetAttributeValue("href", null))
+                                              .Where(u => !String.IsNullOrEmpty(u)).ToList();
+
+            return linkedPages;
+        }
+
+
+        //public static List<String> GetContentFromWebsite(List<String> htmlSource)
+        //{
+        // 
+        //}
+
+        public static void  DownloadContentFromWebsite(List<String> htmlSource)
+        {
+            using (WebClient client = new WebClient()) // WebClient class inherits IDisposable
+            {
+                client.DownloadFile("http://yoursite.com/page.html", @"C:\localfile.html");
+
+                //// Or you can get the file content without saving it:
+                //string htmlCode = client.DownloadString("http://yoursite.com/page.html");
+                ////...
+            }
+        }
+    }
+}
---
+++ SitesDownloder/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using LinksSearch;
+
+namespace SitesDownloder
+{
+    class Program
+    {
+        enum resourseExtentions { gif, jpeg, jpg, pdf, html };
+        public static void Main(string[] args)
+        {
+            //const int depthRestriction = 5;
+
+            //Console.WriteLine("Enter the URL: ");
+            //string url = Console.ReadLine();
+
+            //Console.WriteLine("Enter the path (location on your PC): ");
+            //string path = Console.ReadLine();
+
+            //Console.WriteLine("Select the type of domain restriction: ");
+            //Console.WriteLine("No restrictions - 1");
+            //Console.WriteLine("In current domain only - 2");
+            //Console.WriteLine("Lower than initial URL - 3");
+            //string domainRestriction = Console.ReadLine();
+
+            Links website = new Links("http://stackoverflow.com/", @"C:\");
+            var listOfLinks = website.GetLinksFromWebsite();
+
+            Console.ReadLine();
+        }
+    }
+}
